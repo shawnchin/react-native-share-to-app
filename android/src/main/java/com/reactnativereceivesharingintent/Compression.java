3dfa2a610af9b98f5d8344e7782841f129e5ab42
Shawn <shawn@qwil.io>
2025-02-20 14:11:29 +0000

    refactor + add image compression

---
+++ android/src/main/java/com/reactnativereceivesharingintent/Compression.java
@@ -0,0 +1,105 @@
+package com.reactnativereceivesharingintent;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+
+class Compression {
+    public static final int compressionQuality = 50; // 0-100
+    public static final int maxImageDim = 1200;
+
+    File compressImage(Context context, final String imagePath) throws Exception, IOException, OutOfMemoryError {
+        // Validate image and get dims
+        BitmapFactory.Options imageProps = validateImage(imagePath);
+        int imageWidth = imageProps.outWidth;
+        int imageHeight = imageProps.outHeight;
+
+        // Calculate required scaling
+        ScalingProps scaling = new ScalingProps(imageProps.outWidth, imageProps.outHeight, maxImageDim, maxImageDim);
+        BitmapFactory.Options samplingOptions = new BitmapFactory.Options();
+        samplingOptions.inSampleSize = scaling.sampleSize;
+
+        // Read in image
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, samplingOptions);
+
+        // Use original image exif orientation data to preserve image orientation for the resized bitmap
+        ExifInterface originalExif = new ExifInterface(imagePath);
+        String originalOrientation = originalExif.getAttribute(ExifInterface.TAG_ORIENTATION);
+
+        // Rescale bitmap and write to app cache as compressed JPG
+        bitmap = Bitmap.createScaledBitmap(bitmap, scaling.width, scaling.height, true);
+        File outDir = context.getCacheDir();
+        File outFile = new File(outDir, UUID.randomUUID() + ".jpg");
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(outFile));
+        bitmap.compress(Bitmap.CompressFormat.JPEG, compressionQuality, os);
+        // Don't set unnecessary exif attribute
+        if (shouldSetOrientation(originalOrientation)) {
+            ExifInterface exif = new ExifInterface(outFile.getAbsolutePath());
+            exif.setAttribute(ExifInterface.TAG_ORIENTATION, originalOrientation);
+            exif.saveAttributes();
+        }
+
+        os.close();
+        bitmap.recycle();
+
+        return outFile;
+    }
+
+    private BitmapFactory.Options validateImage(String path) throws Exception {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true; // get image attributes without loading image in mem
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        options.inDither = true;
+
+        BitmapFactory.decodeFile(path, options); // returns null and updates options because inJustDecodeBounds
+
+        if (options.outMimeType == null || options.outWidth == 0 || options.outHeight == 0) {
+            throw new Exception("Invalid image provided");
+        }
+
+        return options;
+    }
+
+    private boolean shouldSetOrientation(String orientation) {
+        return !orientation.equals(String.valueOf(ExifInterface.ORIENTATION_NORMAL))
+                && !orientation.equals(String.valueOf(ExifInterface.ORIENTATION_UNDEFINED));
+    }
+}
+
+class ScalingProps {
+    float scale;
+    int width;
+    int height;
+
+    // sampleSize determines subsampling ratio to reduce size of original image.
+    // Value has to be a power of 2.
+    // https://learn.microsoft.com/en-us/dotnet/api/android.graphics.bitmapfactory.options.insamplesize?view=net-android-34.0
+    int sampleSize;
+
+    public ScalingProps(int originalWidth, int originalHeight, int maxWidth, int maxHeight) {
+        scale = Math.max((float)originalWidth / maxWidth, (float)originalHeight / maxHeight);
+
+        if (scale <= 1) {
+            sampleSize = 1;
+            scale = 1;
+            width = originalWidth;
+            height = originalHeight;
+        } else {
+            sampleSize = 2;
+            while (sampleSize < scale) {
+                sampleSize *= 2;
+            }
+            width = Math.round(originalWidth / scale);
+            height = Math.round(originalHeight / scale);
+        }
+    }
+}
--- android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
+++ android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 
 
 import android.os.Build;
+
 import androidx.annotation.RequiresApi;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -14,48 +16,54 @@ import com.facebook.react.bridge.ReactMethod;
 
 
 public class ReceiveSharingIntentModule extends ReactContextBaseJavaModule {
-  public final String Log_Tag = "ReceiveSharingIntent";
+    public final String Log_Tag = "ReceiveSharingIntent";
 
-  private final ReactApplicationContext reactContext;
-  private ReceiveSharingIntentHelper receiveSharingIntentHelper;
+    private final ReactApplicationContext reactContext;
+    private ReceiveSharingIntentHelper receiveSharingIntentHelper;
 
-  public ReceiveSharingIntentModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-    Application applicationContext = (Application) reactContext.getApplicationContext();
-    receiveSharingIntentHelper = new ReceiveSharingIntentHelper(applicationContext);
-  }
-
-
-  protected void onNewIntent(Intent intent) {
-    Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
-    mActivity.setIntent(intent);
-  }
-
-  @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-  @ReactMethod
-  public void getFileNames(Promise promise){
-    Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
-    Intent intent = mActivity.getIntent();
-    receiveSharingIntentHelper.sendFileNames(reactContext, intent, promise);
-    // mActivity.setIntent(null);
-  }
-
-  @ReactMethod
-  public void clearFileNames(){
-    Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
-    Intent intent = mActivity.getIntent();
-    if (intent != null) {
-      receiveSharingIntentHelper.clearFileNames(intent);
+    public ReceiveSharingIntentModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        Application applicationContext = (Application) reactContext.getApplicationContext();
+        receiveSharingIntentHelper = new ReceiveSharingIntentHelper(applicationContext);
     }
-  }
 
 
-  @Override
-  public String getName() {
-    return "ReceiveSharingIntent";
-  }
+    protected void onNewIntent(Intent intent) {
+        Activity mActivity = getCurrentActivity();
+        if (mActivity == null) {
+            return;
+        }
+        mActivity.setIntent(intent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @ReactMethod
+    public void getFileNames(Promise promise) {
+        Activity mActivity = getCurrentActivity();
+        if (mActivity == null) {
+            return;
+        }
+        Intent intent = mActivity.getIntent();
+        receiveSharingIntentHelper.sendFileNames(reactContext, intent, promise);
+        // mActivity.setIntent(null);
+    }
+
+    @ReactMethod
+    public void clearFileNames() {
+        Activity mActivity = getCurrentActivity();
+        if (mActivity == null) {
+            return;
+        }
+        Intent intent = mActivity.getIntent();
+        if (intent != null) {
+            receiveSharingIntentHelper.clearFileNames(intent);
+        }
+    }
+
+
+    @Override
+    public String getName() {
+        return "ReceiveSharingIntent";
+    }
 }
